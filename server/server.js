b69ab7f686938c67e062b78d9f4d1415e617a547
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2023-01-20 10:23:24 +0800

    Connect server to database

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+const dbConnect = require("./db/dbConnect");
 
 app.use(cors());
 
 const server = http.createServer(app);
+dbConnect();
 
 const io = new Server(server, {
   cors: {
