45c358f72a4fd73d4e741056cf48b8316f9acc9f
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2023-02-02 01:53:43 +0800

    Setup Login and token verification in backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
+require("dotenv").config();
 
 const signup = async (req, res, next) => {
   const { username, firstName, lastName, email, password } = req.body;
@@ -54,7 +56,56 @@ const login = async (req, res, next) => {
   if (!isPasswordCorrect) {
     return res.status(400).json({ message: "Invalid Email/Password" });
   }
+
+  const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "24h",
+  });
+
+  return res
+    .status(200)
+    .json({ message: "Successfully Logged In", user: existingUser, token });
+};
+
+const verifyToken = (req, res, next) => {
+  const headers = req.headers[`authorization`];
+
+  const token = headers.split(" ")[1];
+
+  if (!token) {
+    res.status(404).json({ message: "No token found" });
+  }
+
+  jwt.verify(String(token), process.env.JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(400).josn({ message: "Invalid Token" });
+    }
+
+    console.log(user.id);
+    req.id = user.id;
+  });
+
+  next();
+};
+
+const getUser = async (req, res, next) => {
+  const userId = req.id;
+
+  let user;
+
+  try {
+    user = await User.findById(userId, "-password");
+  } catch (err) {
+    return new Error(err);
+  }
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  return res.status(200).json({ user });
 };
 
 exports.signup = signup;
 exports.login = login;
+exports.verifyToken = verifyToken;
+exports.getUser = getUser;
--- server/router/userRoutes.js
+++ server/router/userRoutes.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
-const { signup, login } = require("../controllers/userController");
+const {
+  signup,
+  login,
+  verifyToken,
+  getUser,
+} = require("../controllers/userController");
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/user", verifyToken, getUser);
 
 module.exports = router;
